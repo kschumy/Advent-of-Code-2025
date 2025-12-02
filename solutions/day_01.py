51715145c14c41c710a70acb881c05a1fa655812
Kirsten Schumy <kirsten.schumy@gmail.com>
2025-12-01 19:19:35 -0800

    Cleaned up day 01

--- solutions/day_01.py
+++ solutions/day_01.py
@@ -11,35 +11,23 @@ ROTATION_AMOUNT = MAX_NUM + 1  # 0-99 inclusive
 LEFT = "L"
 
 curr_num = START_NUM
-
 passed_zero_count = 0
 is_zero_count = 0
 
 for line in read_lines(INPUT_FILENAME):
     is_left = line[0] == LEFT
     amount = int(line[1:])
-    
     passed_zero_count += amount // ROTATION_AMOUNT
     remainder = amount % ROTATION_AMOUNT
-    
-    if is_left:
-        if curr_num > remainder:
-            curr_num -= remainder
-        else:
-            if curr_num != 0:
+    if remainder:
+        if is_left: # direction is "L"
+            if curr_num != 0 and (curr_num - remainder) <= 0:
                 passed_zero_count += 1
-            beyond_zero = remainder - curr_num
-            curr_num = ROTATION_AMOUNT - beyond_zero
-            if curr_num > MAX_NUM:
-                curr_num = 0
-    else: # direction is "R"
-        if curr_num + remainder <= MAX_NUM:
-            curr_num += remainder
-        else:
-            if curr_num != 0:
+            curr_num = (curr_num - remainder) % ROTATION_AMOUNT
+        else: # direction is "R"
+            if (curr_num + remainder) > MAX_NUM:
                 passed_zero_count += 1
-            beyond_zero = curr_num + remainder
-            curr_num = beyond_zero - ROTATION_AMOUNT
+            curr_num = (curr_num + remainder) % ROTATION_AMOUNT
     if curr_num == 0:
         is_zero_count += 1
 

86f52a348cfd5a7b5b04dc66f3928d209917e75e
Kirsten Schumy <kirstenschumy@Kirstens-MacBook-Air.local>
2025-12-01 19:24:52 -0800

    Additional refactor on day 01

--- solutions/day_01.py
+++ solutions/day_01.py
@@ -1,35 +1,52 @@
 from utils.process_input import read_lines
-
+INPUT_FILENAME = "day_01.txt"
 PART_ONE_EXPECTED_ANSWER = 1048
 PART_TWO_EXPECTED_ANSWER = 6498
-INPUT_FILENAME = "day_01.txt"
 
-START_NUM = 50
+LEFT = "L"
 MAX_NUM = 99
 MIN_NUM = 0
 ROTATION_AMOUNT = MAX_NUM + 1  # 0-99 inclusive
-LEFT = "L"
+START_NUM = 50
 
-curr_num = START_NUM
-passed_zero_count = 0
-is_zero_count = 0
+def process_moves(lines) -> tuple[int, int]:
+    curr_num = START_NUM
+    passed_zero_count = 0
+    is_zero_count = 0
+    
+    for line in lines:
+        is_left = line[0] == LEFT
+        amount = int(line[1:])
+        
+        passed_zero_count += amount // ROTATION_AMOUNT
+        remainder = amount % ROTATION_AMOUNT
+        
+        if remainder:
+            if is_left: # direction is "L"
+                if curr_num != 0 and (curr_num - remainder) <= 0:
+                    passed_zero_count += 1
+                curr_num = (curr_num - remainder) % ROTATION_AMOUNT
+            else: # direction is "R"
+                if (curr_num + remainder) > MAX_NUM:
+                    passed_zero_count += 1
+                curr_num = (curr_num + remainder) % ROTATION_AMOUNT
+        if curr_num == 0:
+            is_zero_count += 1
 
-for line in read_lines(INPUT_FILENAME):
-    is_left = line[0] == LEFT
-    amount = int(line[1:])
-    passed_zero_count += amount // ROTATION_AMOUNT
-    remainder = amount % ROTATION_AMOUNT
-    if remainder:
-        if is_left: # direction is "L"
-            if curr_num != 0 and (curr_num - remainder) <= 0:
-                passed_zero_count += 1
-            curr_num = (curr_num - remainder) % ROTATION_AMOUNT
-        else: # direction is "R"
-            if (curr_num + remainder) > MAX_NUM:
-                passed_zero_count += 1
-            curr_num = (curr_num + remainder) % ROTATION_AMOUNT
-    if curr_num == 0:
-        is_zero_count += 1
+    return (is_zero_count, passed_zero_count)
 
-for result in [("ONE", PART_ONE_EXPECTED_ANSWER, is_zero_count), ("TWO", PART_TWO_EXPECTED_ANSWER, passed_zero_count)]:
-    print(f"PART {result[0]} ANSWER:\n\tExpected: {result[1]}\n\tActual: {result[2]}\n\tIs Correct: {result[1] == result[2]}\n")
+if __name__ == "__main__":
+    lines = read_lines(INPUT_FILENAME)
+    part1, part2 = process_moves(lines)
+
+    results = [
+        ("ONE", PART_ONE_EXPECTED_ANSWER, part1),
+        ("TWO", PART_TWO_EXPECTED_ANSWER, part2),
+    ]
+    for name, expected, actual in results:
+        print(
+            f"PART {name} ANSWER:\n"
+            f"\tExpected: {expected}\n"
+            f"\tActual:   {actual}\n"
+            f"\tIs Correct: {expected == actual}\n"
+        )

4051c8ca328c32aff456112626d2b7d4d13c637b
Kirsten Schumy <kirsten.schumy@gmail.com>
2025-12-02 17:34:45 -0800

    Added new data processing for day 02

--- utils/process_input.py
+++ utils/process_input.py
@@ -1,5 +1,13 @@
 from pathlib import Path
 
 def read_lines(input_filename: str):
+    data_file = _read_raw(input_filename)
+    return [line for line in data_file.splitlines() if line.strip()]
+
+def split_lines(input_filename: str, delimiter: str = ","):
+    data_file = _read_raw(input_filename)
+    return [line.split(delimiter) for line in data_file.splitlines() if line.strip()]
+
+def _read_raw(input_filename: str):
     data_file = Path(__file__).resolve().parent.parent / "input" / input_filename
-    return [line for line in data_file.read_text().splitlines() if line.strip()]
+    return data_file.read_text()

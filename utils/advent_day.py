62aafe76daac828b5191f1151ae774b180392547
Kirsten Schumy <kirsten.schumy@gmail.com>
2025-12-04 15:24:23 -0800

    Moved reader and results functionality

--- utils/advent_day.py
+++ utils/advent_day.py
@@ -1,5 +1,5 @@
 from utils.part import Part
-from utils.process_input import get_expected_answers
+from utils.reader import read_answers
 
 class AdventDay:
     def __init__(self, day: int, is_example: bool=False):
@@ -8,15 +8,27 @@ class AdventDay:
         self.is_example = is_example
         self.filename = f"day_{self.day_str}{'_example' if self.is_example else ''}.txt"
 
+    def get_expected_answer(self, input_filename: str, part: Part) -> str:
+        answers = self.get_expected_answers(input_filename)
+        return answers[part.value - 1]
+
+    # returns [<part one expected>, <part two expected>]
+    def get_expected_answers(self, input_filename: str) -> list[int]:
+        answers_file = read_answers(input_filename)
+        results = [int(line) for line in answers_file.splitlines() if line.strip()]
+        if len(results) != 2:
+            raise ValueError("Expected answers file must contain exactly two lines")
+        return results
+
     def print_both_results(self, part_one_actual: int, part_two_actual: int):
-        both_parts_expected = get_expected_answers(self.filename)
+        both_parts_expected = self.get_expected_answers(self.filename)
         self._print_result(part_one_actual, both_parts_expected[0], Part.ONE)
         self._print_result(part_two_actual, both_parts_expected[1], Part.TWO)
 
     def print_part_result(self, actual: int, part: Part):
         if part not in [Part.ONE, Part.TWO]:
             raise ValueError(f"part must be {Part.ONE} or {Part.TWO}")
-        both_parts_expected = get_expected_answers(self.filename)
+        both_parts_expected = self.get_expected_answers(self.filename)
         self._print_result(actual, both_parts_expected[part.value - 1], part)
 
     def _print_result(self, actual: int, expected: int, part: Part):
--- utils/process_input.py
+++ utils/process_input.py
@@ -1,17 +1,13 @@
-from pathlib import Path
-from utils.part import Part
-
-ANSWERS_DIR = "answers"
-INPUT_DIR = "input"
+from utils.reader import read_input
 
 # Day 01, 03
 def read_lines(input_filename: str) -> list[str]:
-    data_file = _read_raw_input(input_filename)
+    data_file = read_input(input_filename)
     return [line for line in data_file.splitlines() if line.strip()]
 
 # Day 02
 def split_lines(input_filename: str, delimiter: str = ",") -> list[list[str]]:
-    data_file = _read_raw_input(input_filename)
+    data_file = read_input(input_filename)
     return [line.split(delimiter) for line in data_file.splitlines() if line.strip()]
 
 # Day 04
@@ -20,7 +16,7 @@ def read_lines_to_matrix_with_borders(
     border_len: int = 1,
     border_char: str = ".",
 ) -> list[list[str]]:
-    lines = _read_raw_input(input_filename).splitlines()
+    lines = read_input(input_filename).splitlines()
     border_row = [border_char] * (len(lines[0]) + 2 * border_len)
     padding = [border_char] * border_len
     arr = []
@@ -38,22 +34,3 @@ def read_lines_to_matrix_with_borders(
         arr.append(border_row.copy())
 
     return arr
-
-def get_expected_answer(input_filename: str, part: Part) -> str:
-    answers = get_expected_answers(input_filename)
-    return answers[part.value - 1]
-
-# returns [<part one expected>, <part two expected>]
-def get_expected_answers(input_filename: str) -> list[int]:
-    answers_file = _read_raw(ANSWERS_DIR, input_filename)
-    results = [int(line) for line in answers_file.splitlines() if line.strip()]
-    if len(results) != 2:
-        raise ValueError("Expected answers file must contain exactly two lines")
-    return results
-
-def _read_raw_input(input_filename: str) -> str:
-    return _read_raw(INPUT_DIR, input_filename)
-
-def _read_raw(parent_dir: str, input_filename: str) -> str:
-    data_file = Path(__file__).resolve().parent.parent / parent_dir / input_filename
-    return data_file.read_text()

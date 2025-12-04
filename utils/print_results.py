0cdb5cc6ef9acc92324e9edca51b91207bd2476f
Kirsten Schumy <kirsten.schumy@gmail.com>
2025-12-03 18:43:02 -0800

    Deleted no longer needed class for printing results

--- utils/print_results.py
+++
@@ -1,9 +0,0 @@
-class PrintResults:
-    @staticmethod
-    def print_result(day, part, expected, actual):
-        print(
-            f"\nDAY {day}, PART {part} SOLUTION:\n"
-            f"\tExpected: {expected}\n"
-            f"\tActual:   {actual}\n"
-            f"\tIs Correct?: {expected == actual}\n"
-        )

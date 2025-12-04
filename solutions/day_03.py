6eebcccaede7a593d46253564d57fa64611bc7ef
Kirsten Schumy <kirsten.schumy@gmail.com>
2025-12-03 16:34:42 -0800

    Likely final refactor and clean up of day 03

--- solutions/day_03.py
+++ solutions/day_03.py
@@ -1,34 +1,11 @@
-from collections import deque
 from utils.process_input import read_lines
-
-# INPUT_FILENAME = "day_03_example.txt"
-# PART_ONE_EXPECTED_ANSWER = 357
-# PART_TWO_EXPECTED_ANSWER = 3121910778619
 
 INPUT_FILENAME = "day_03.txt"
 PART_ONE_EXPECTED_ANSWER = 17324
 PART_TWO_EXPECTED_ANSWER = 171846613143331
 
-
-
-## PART ONE SOLUTION ###
-# lines = read_lines(INPUT_FILENAME)
-# # print(lines)
-# total = 0
-# for line in lines:
-#     curr_total = [line[0], line[1]]
-#     # print(curr_total)
-#     for i in range(1, len(line) - 1):
-#         if line[i] > curr_total[0]:
-#             curr_total = [line[i], line[i + 1]]
-#         elif line[i] > curr_total[1]:
-#             curr_total[1] = line[i]
-#     curr_total[1] = max(curr_total[1], line[-1])
-#     total += int("".join(curr_total))
-# print(total)
-# ## END PART ONE SOLUTION ###
-
-DIGIT_SPAN = 12
+PART_ONE_DIGIT_SPAN = 2
+PART_TWO_DIGIT_SPAN = 12
 
 def largest_possible_joltage(s: str, digit_span: int) -> int:
     n = len(s)
@@ -40,28 +17,17 @@ def largest_possible_joltage(s: str, digit_span: int) -> int:
             stack.append(ch)
     return int(''.join(stack[:digit_span]))
 
-def get_part_one_and_part_two_answers(lines):
-    part_one_total = 0
-    part_two_total = 0
+def get_total_voltage(lines, digit_span):
+    total = 0
     for line in lines:
-        part_one_total += largest_possible_joltage(line, 2)
-        part_two_total += largest_possible_joltage(line, DIGIT_SPAN)
-    return part_one_total, part_two_total
+        total += largest_possible_joltage(line, digit_span)
+    return total
 
 if __name__ == "__main__":
-    # total = 0
-    # lines = read_lines(INPUT_FILENAME)
-    # for line in lines:
-    #     total += largest_possible_joltage(line, DIGIT_SPAN)
-    # print(total)
-    # print(f"EXPECTED ANSWER: {PART_TWO_EXPECTED_ANSWER}")
-    # print(f"ACTUAL ANSWER: {total}")
-    # print(f"ARE THEY EQUAL? {total == PART_TWO_EXPECTED_ANSWER}")
-
-    part_one_result, part_two_result = get_part_one_and_part_two_answers(read_lines(INPUT_FILENAME))
+    lines = read_lines(INPUT_FILENAME)
     results = [
-        ("ONE", PART_ONE_EXPECTED_ANSWER, part_one_result),
-        ("TWO", PART_TWO_EXPECTED_ANSWER, part_two_result),
+        ("ONE", PART_ONE_EXPECTED_ANSWER, get_total_voltage(lines, PART_ONE_DIGIT_SPAN)),
+        ("TWO", PART_TWO_EXPECTED_ANSWER, get_total_voltage(lines, PART_TWO_DIGIT_SPAN)),
     ]
     for name, expected, actual in results:
         print(

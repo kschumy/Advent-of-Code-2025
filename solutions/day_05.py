8d1283f24d64eca44932ad6cf39a920ee32e1adc
Kirsten Schumy <kirsten.schumy@gmail.com>
2025-12-05 16:46:55 -0800

    Included day 05 part 2 solution that works but is a mess

--- solutions/day_05.py
+++ solutions/day_05.py
@@ -8,19 +8,36 @@ day = AdventDay(DAY, IS_EXAMPLE)
 
 ranges, values = read_input_with_ranges_and_integers(day.filename)
 print(ranges)
-sorted_ranges = sorted(ranges, key=lambda x: (x[1], x[0]))
+sorted_ranges = sorted(ranges, key=lambda x: [x[1], x[0]])
 print(sorted_ranges)
 values.sort()
 print(values)
 
-fresh_count = 0
-# range_index = len(sorted_ranges) - 1
-for i in range(len(values) - 1, -1, -1):
-    val = values[i]
-    range_index = len(sorted_ranges) - 1
-    while range_index >= 0 and not (ranges[range_index][0] <= val <= ranges[range_index][1]) :
-        range_index -= 1
-    if range_index >= 0 and ranges[range_index][0] <= val <= ranges[range_index][1]:
-        print(f"fresh: {val}")
-        fresh_count += 1
-print(fresh_count)
+combined_ranges = [sorted_ranges.pop()]
+while sorted_ranges:
+    curr = sorted_ranges.pop()
+    prev_start, prev_end = combined_ranges[-1]
+    if prev_start <= curr[1] <= prev_end:
+        combined_ranges[-1][0] = min(prev_start, curr[0])
+    else:
+        combined_ranges.append(curr)
+
+total = 0
+for c in combined_ranges:
+    total += c[1] - c[0] + 1
+print(total)
+
+
+### PART ONE ###
+# fresh_count = 0
+# # range_index = len(sorted_ranges) - 1
+# for i in range(len(values) - 1, -1, -1):
+#     val = values[i]
+#     range_index = len(sorted_ranges) - 1
+#     while range_index >= 0 and not (ranges[range_index][0] <= val <= ranges[range_index][1]) :
+#         range_index -= 1
+#     if range_index >= 0 and ranges[range_index][0] <= val <= ranges[range_index][1]:
+#         print(f"fresh: {val}")
+#         fresh_count += 1
+# print(fresh_count)
+### END PART ONE ###

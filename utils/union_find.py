5d07777d283d51946399e07e13f02f7b64d7827b
Kirsten Schumy <kirsten.schumy@gmail.com>
2025-12-08 18:38:53 -0800

    Added is_unified to union find for optimization

--- utils/union_find.py
+++ utils/union_find.py
@@ -1,80 +1,19 @@
 class UnionFind:
     def __init__(self, n: int):
         self.parents = list(range(n))
+        self.num_of_elements = n
 
-    def find(self, val:int):
-        if self.parents[val] == val:
-            return self.parents[val]
-        return self.find(self.parents[val])
-    
-    # return True if merged and False if already in the same group
-    def union(self, val_1: int, val_2: int) -> bool:
+    def find(self, val:int) -> int:
+        if self.parents[val] != val:
+            self.parents[val] = self.find(self.parents[val])
+        return self.parents[val]
+
+    def union(self, val_1: int, val_2: int) -> None:
         root_1 = self.find(val_1)
         root_2 = self.find(val_2)
-        if root_1 == root_2:
-            return False
-        self.parents[root_2] = root_1
-        return True
+        if root_1 != root_2:
+            self.parents[root_2] = root_1
+            self.num_of_elements -= 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # def __init__(self, size: int):
-    #     self.parent = list(range(size))
-    #     self.rank = [1] * size
-
-    # def find(self, p: int) -> int:
-    #     if self.parent[p] != p:
-    #         self.parent[p] = self.find(self.parent[p])
-    #     return self.parent[p]
-
-    # def union(self, p: int, q: int) -> None:
-    #     rootP = self.find(p)
-    #     rootQ = self.find(q)
-    #     if rootP != rootQ:
-    #         if self.rank[rootP] > self.rank[rootQ]:
-    #             self.parent[rootQ] = rootP
-    #         elif self.rank[rootP] < self.rank[rootQ]:
-    #             self.parent[rootP] = rootQ
-    #         else:
-    #             self.parent[rootQ] = rootP
-    #             self.rank[rootP] += 1
+    def is_unified(self) -> bool:
+        return self.num_of_elements == 1
